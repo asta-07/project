b1e032fd67d56d03da1b8f9980464765b0098f0d
asta-07 <astab9450@gmail.com>
2026-04-15 01:08:06 -0400

    Update App.py

--- App.py
+++ App.py
@@ -2,4 +2,4 @@ import streamlit as st
 from groq import Groq
 st.set_page.config("Pragyan content Generation",layout="wide")
 st.title("StartzenAI-Content Generator")
-st.image("SDP_AI-
+st.image("ai1.jpg")
